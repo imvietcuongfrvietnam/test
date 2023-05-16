67116dfe2e0228912a0b3661240392f5c8e6640d
Cuong <Cuong.tv224941@sis.hust.edu.vn>
2023-05-16 22:48:11 +0700

    Test

---
+++ cachamtoanhoc.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+int main(){
+	//cac ham toan hoc trong c++
+	//khai bao thu vien cmath hoac math.h
+	
+	//trinh viet cuong december 19,2022
+	
+  cout<<"Name"<<setw(11)<<"Depict\n";
+  float 
+   x;
+   cin>>setw(1)>>x;
+   cout<<setw(1)<<x;  
+  
+}
